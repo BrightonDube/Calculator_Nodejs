7c0dba65a7c3e6a9c04ebe675c41ce2721d7f950
BrightonDube <BrightonDube@users.noreply.github.com>
2020-03-27 14:07:48 +0200

    response updated

--- calculator.js
+++ calculator.js
@@ -1,10 +1,11 @@
 //jshint esversion: 6
 const express = require("express");
 const app = express();
+const path = require('path')
 const port = 3500;
 
 app.get('/', (req, res)=>{
-    res.send("Hello World!");
+    res.sendFile(`${path.join(__dirname, "/index.html")}`);
 });
 
 app.listen(port, ()=>{
