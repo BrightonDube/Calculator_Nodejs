fbf7437b4d33702df1afec2a44aa4d0f24a47795
BrightonDube <BrightonDube@users.noreply.github.com>
2020-03-27 17:47:21 +0200

    added more operators

--- calculator.js
+++ calculator.js
@@ -2,6 +2,23 @@
 const express = require("express");
 const app = express();
 const port = 3500;
+
+
+function add(num1, num2){
+    return num1 + num2;}
+function multiply(num1, num2) { 
+   return num1 * num2;}
+function divide(num1, num2) { 
+   return num1 / num2;}
+function subtract(num1, num2) {
+   return num1 - num2;}
+
+// These are higher order functions that take in other functions as input.
+const calculator = function(num1, num2, operator){
+   
+    return operator(num1, num2);
+   // operator is a function that performs the required computation.;
+};
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res)=>{
@@ -11,11 +28,34 @@ app.get('/', (req, res)=>{
 
 app.post("/", (req, res)=>{    
     let num1 = parseInt(req.body.num1 ,10);
-    let num2 = parseInt(req.body.num2, 10);
+    let num2 = Number(req.body.num2);
+    let oper = req.body.operator;
+    let answer;
+
+    switch (oper){
+        case "+":
+            answer = calculator(num1, num2, add);
+            break;
+        
+        case "-":
+            answer = calculator(num1, num2, subtract);
+            break;
+        
+        case "*":
+            answer = calculator(num1, num2, multiply);
+            break;
+
+        case "/":
+            answer = calculator(num1, num2, divide);
+            break;
+
+        default:
+            console.log("You did not enter a correct operator");
+            break;
+    }
     
-    let answer = num1 + num2;
-    console.log(answer);
-    res.end();
+    
+    res.send(`The result is ${answer}`);
 });
 
 app.listen(port, ()=>{
