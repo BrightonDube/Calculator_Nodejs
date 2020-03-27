73b278b0decb2f992e1f038402bdd527846d93a2
BrightonDube <BrightonDube@users.noreply.github.com>
2020-03-27 15:47:34 +0200

    parsing form data

--- calculator.js
+++ calculator.js
@@ -1,11 +1,21 @@
 //jshint esversion: 6
 const express = require("express");
 const app = express();
-const path = require('path')
 const port = 3500;
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res)=>{
-    res.sendFile(`${path.join(__dirname, "/index.html")}`);
+    res.sendFile(`${__dirname}/index.html`);
+
+});
+
+app.post("/", (req, res)=>{    
+    let num1 = parseInt(req.body.num1 ,10);
+    let num2 = parseInt(req.body.num2, 10);
+    
+    let answer = num1 + num2;
+    console.log(answer);
+    res.end();
 });
 
 app.listen(port, ()=>{
